ed201c293be32c41ad748522a4e495db15a1a8e6
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-05-02 16:44:40 +0200

    aggiunta ciclo per i numeri da 1 a 100

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//1. Scrivo il ciclo per stampare i numeri da 1 a 100
+for (let i = 1; i <= 100; i++){
+    console.log (i);
+}
+//2. Lo stampo in console
+//3. °Stampo "Fizz" per i multipli di 3
+//   °Stampo "Buzz" per i multipli di 5
+//   °Stampo "FizzBuzz" per i multipli di entrambi
+//4. Stampo la lista nell'html
+//5. Applico un po di style css
+     
