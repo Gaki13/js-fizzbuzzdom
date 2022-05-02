f07e8787372ff1cb4d9a5f4616662dc4f823dfe0
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-05-02 18:35:27 +0200

    aggiunta condizione principale per determinare il multiplo del numero

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 //1. Scrivo il ciclo per stampare i numeri da 1 a 100
 for (let i = 1; i <= 100; i++){
-    console.log (i);
-}
-//2. Lo stampo in console
-//3. °Stampo "Fizz" per i multipli di 3
-//   °Stampo "Buzz" per i multipli di 5
-//   °Stampo "FizzBuzz" per i multipli di entrambi
+    //2. Lo stampo in console
+    //3. °Stampo "Fizz" per i multipli di 3
+    //   °Stampo "Buzz" per i multipli di 5
+    //   °Stampo "FizzBuzz" per i multipli di entrambi
+    if ((i % 3 === 0) && (i % 5 === 0)){
+        console.log ('FizzBuzz')
+    } else if (i % 3 === 0){
+    console.log ('Fizz')
+    } else if (i % 5 === 0) {
+        console.log ('Buzz')
+    } else {
+        console.log (i)
+    };
+};
+    
 //4. Stampo la lista nell'html
 //5. Applico un po di style css
      
